33dd4cf132c55419048fa34dca88acb2ad17548b
RubenSugianto <ruben.sugianto@gmail.com>
2025-03-04 22:00:34 +0700

    Making Projects Selectable & Viewing Project Details & Handling Project Deletion

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import {useState} from 'react';
 import NewProject from "./components/NewProject.jsx";
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import SelectedProject from './components/SelectedProject.jsx';
 
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
     projects: []
   });
+
+  function handleSelectProject(id) {
+    setProjectsState(prevState => {
+      return {...prevState, selectedProjectId: id };
+    });
+  }
 
   function handleStartAddProject() {
     setProjectsState(prevState => {
@@ -38,7 +45,23 @@ function App() {
     });
   }
 
-  let content;
+  function handleDeleteProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+      };
+    });
+  }
+
+  const selectedProject = projectsState.projects.find(
+    (project) => project.id === projectsState.selectedProjectId
+  );
+
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>;
 
   if(projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>
@@ -51,6 +74,7 @@ function App() {
       <ProjectsSidebar 
         onStartAddProject={handleStartAddProject} 
         projects={projectsState.projects}
+        onSelectProject={handleSelectProject}
       />
       {content}
     </main>
